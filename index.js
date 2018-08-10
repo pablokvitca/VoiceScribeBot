5252b2e5f48b5a6faa873066ec51aabe9262adc9
Pablo Kvitca <pablokvitca@gmail.com>
2018-08-10 03:43:08 -0300

    initial, ugly, command testing

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 // Configure DOTENV
 require('dotenv').config();
+const _ = require('lodash');
 
 const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.TELEGRAM_CHATBOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'));
+const languages = [
+    "english",
+    "spanish"
+]
 
-bot.command('oldschool', (ctx) => ctx.reply('Hello'));
-bot.command('modern', ({ reply }) => reply('Yo'));
-bot.command('hipster', Telegraf.reply('λ'));
+let currentLanguage = languages[0];
+
+bot.start((ctx) => ctx.reply('Welcome! TODO: welcome message'));
+bot.help((ctx) => ctx.reply('TODO: help message'));
+
+bot.command('setdefaultlanguage', (ctx) => {
+
+    function parseMessage(message, langs) {
+        console.log(message.text);
+        const rawArgs = message.text.replace('/setdefaultlanguage ', '');
+        console.log(rawArgs);
+        const args = rawArgs.split(" ");
+        console.log(args);
+        let newLang;
+        langs.forEach((lang, key) => {
+            if (args[0] == lang) {
+                newLang = key;
+            } else {
+                console.log("IS NOT " + lang)
+            }
+        });
+        console.log(newLang);
+        return newLang;
+    }
+
+    const newLang = parseMessage(ctx.message, languages);
+    if (newLang !== undefined) {
+        currentLanguage = languages[newLang];
+        ctx.reply(`Done! Default language set to: ${currentLanguage}`);
+    } else {
+        ctx.reply(`Sorry, could not do that. Language is still: ${currentLanguage}`);
+    }
+});
+bot.command('transcribe', (ctx) => {
+    ctx.reply('Transcribing last received audio...');
+    //TODO: 
+});
+bot.command('settag', (ctx) => {
+    ctx.reply('Transcribing last received audio...');
+    //TODO: 
+});
+bot.command('clearmemory', (ctx) => {
+    ctx.reply('Clearing audio memory');
+    //TODO: 
+});
+bot.command('clearaudio', (ctx) => {
+    ctx.reply('Clearing last audio...');
+    //TODO: 
+});
+bot.command('givesocks', ({ reply }) => reply('Dobby is free.'));
 
 bot.startPolling();
